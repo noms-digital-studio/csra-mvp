68bfe30d579f672e6b7ef4ee4f2e85b19edd8f51
Steven Bapaga <sbapaga@gmail.com>
2017-03-03 13:57:09 +0000

    Add sourceMaps for assets in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,28 +34,50 @@ module.exports = [
                     use: [
                         "style-loader",
                         "css-loader",
+                        "resolve-url-loader",
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                            plugins: function () {
+                                return [
+                                require('precss'),
+                                require('autoprefixer')
+                                ];
+                            }
+                            }
+                        }
                     ]
                 },
                 {
                     test: /\.scss/,
                     use: [
                         "style-loader",
-                        "css-loader",
+                        {
+                            loader: "css-loader", 
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        "resolve-url-loader",
                         {
                             loader: "sass-loader",
                             options: {
+                                sourceMap: true,
                                 includePaths: [
                                     path.join(__dirname, "node_modules/govuk-elements-sass/public/sass"),
-                                    path.join(__dirname, "node_modules/govuk_frontend_toolkit/stylesheets"),
+                                    path.join(__dirname, "node_modules/govuk_frontend_toolkit/stylesheets")                                    
                                 ]
                             }
                         }
+                    
                     ]
                 },
                 {
                     test: /\.png$/,
                     loader: "file-loader",
-                    options: { prefix: "img/" }
+                    options: { 
+                        prefix: "img/"
+                    }
                 }
             ]
         },
