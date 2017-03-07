3e4a080bc23ccc5b1a525cc6b769a40368d900b6
Steven Bapaga <sbapaga@gmail.com>
2017-03-07 14:57:34 +0000

    Add 404 page

--- src/app/Router.js
+++ src/app/Router.js
@@ -10,14 +10,10 @@ import { syncHistoryWithStore } from 'react-router-redux';
 
 import Layout from './templates/Main';
 
-//Pages
 import LoadData from './pages/LoadData';
 import Signin from './pages/Singin';
 import Question from './pages/Question';
-
-
-//Routes
-import { SIGNIN, ASSESSMENT } from './constants/routes';
+import Error404 from './pages/Error404';
 
 
 export default (store) => {
@@ -28,11 +24,10 @@ export default (store) => {
             <Router history={history}>
                 <Route component={Layout}>
                     <Route path="/" name="Sign in" component={Signin} />
-                    <Route path="load-data" name="load-data" component={LoadData} />                    
-                    <Route path={SIGNIN} name="Sign in" component={Signin} />
-                    <Route name="assessment">
-                        <Route path="assessment/:section" component={Question} />
-                    </Route>
+                    <Route path="/load-data" name="load-data" component={LoadData} />                    
+                    <Route path="/signin" name="Sign in" component={Signin} />
+                    <Route path="/assessment/:section" component={Question} />
+                    <Route path="*" name="404"  component={Error404}/>
                 </Route>
             </Router>
         </Provider>
