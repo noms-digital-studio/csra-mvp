83a33aae5680c970436e5ceb67723a1c5323a36e
Steven Bapaga <sbapaga@gmail.com>
2017-03-08 15:23:36 +0000

    @romidane @retroredge: Add Viper score calculator
    Connected to https://dsdmoj.atlassian.net/browse/CSRA-150

--- tests/services/index.spec.js
+++ tests/services/index.spec.js
@@ -1,13 +1,56 @@
-
-
+import { calculateRiskFor } from '../../src/javascript/services'
 
 describe('Services', () => {
-    
-    describe('#riskCalculator', () => {
-    
+
+    describe('#calculateRiskFor', () => {
+        const viperScores = [
+            {
+                "nomisId": "nomisIdForLowRiskPerson1",
+                "viperScore": "0.00"
+            },
+            {
+                "nomisId": "nomisIdForLowRiskPerson2",
+                "viperScore": "0.50"
+            },
+            {
+                "nomisId": "nomisIdForMediumRiskPerson1",
+                "viperScore": "0.51"
+            },
+
+            {
+                "nomisId": "nomisIdForMediumRiskPerson2",
+                "viperScore": "0.75"
+            },
+            {
+                "nomisId": "nomisIdForHighRiskPerson1",
+                "viperScore": "0.76"
+            },
+            {
+                "nomisId": "nomisIdForHighRiskPerson2",
+                "viperScore": "1.0"
+            }            
+        ];
+
         it('returns a low risk rating', () => {
-            expect(true).to.equal(false);
-            // expect(riskCalculator.riskFor('foo')).to.equal('bar');
+            expect(calculateRiskFor('nomisIdForLowRiskPerson1', viperScores)).to.equal('low');
+            expect(calculateRiskFor('nomisIdForLowRiskPerson2', viperScores)).to.equal('low');            
         });
+
+        it('returns a med risk rating', () => {
+            expect(calculateRiskFor('nomisIdForMediumRiskPerson1', viperScores)).to.equal('medium');
+            expect(calculateRiskFor('nomisIdForMediumRiskPerson2', viperScores)).to.equal('medium');            
+        });
+
+        it('returns a high risk rating', () => {
+            expect(calculateRiskFor('nomisIdForHighRiskPerson1', viperScores)).to.equal('high');
+            expect(calculateRiskFor('nomisIdForHighRiskPerson2', viperScores)).to.equal('high');            
+        });
+
+        context('when a score doesn\'t exist for a given nomisId', () => {
+            it('returns unknown', () => {
+                expect(calculateRiskFor('nomisIdForUnknown', viperScores)).to.equal('unknown');            
+            })
+        });
+        
     });
 });
