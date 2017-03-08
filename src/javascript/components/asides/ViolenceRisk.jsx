744737687abcd7eacb42e8f34ec1ef6a906af26f
Steven Bapaga <sbapaga@gmail.com>
2017-03-08 16:12:43 +0000

    Integrate viperscore calculation into <ViolenceRisk />

--- src/javascript/components/asides/ViolenceRisk.jsx
+++ src/javascript/components/asides/ViolenceRisk.jsx
@@ -1,30 +1,34 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { calculateRiskFor } from '../../services';
 
-class ViolenceRisk extends Component {
-    componentDidMount() {
+const ViolenceRisk = ({ rating }) => {
+    const riskScoreClasses = classnames({
+        'c-risk-threat': true,
+        'heading-large': true,
+        [`c-risk-threat--${rating}`]: true
+    });
 
-    }
-
-    render() {
-        return (
-            <aside className="govuk-related-items" role="complementary">
-                <h3 className="heading-medium u-margin-top-default" id="subsection-title">Risk of Violence</h3>
-                <p><strong>LOW</strong></p>
-            </aside>
-        )
-    }
+    return (
+        <aside className="govuk-related-items" role="complementary">
+            <h3 className="heading-medium u-margin-top-default" id="subsection-title">Risk of Violence</h3>
+            <p><span data-viper-rating={rating} className={riskScoreClasses}>{rating}</span></p>
+        </aside>
+    );
 }
 
 
 ViolenceRisk.propTypes = {
-    prisonerId: PropTypes.string,
-
-}
+    rating: PropTypes.string,
+};
 
 const mapStateToProps = (state, ownProps) => ({ 
-    prisonerId: state.prisoner.current.nomsID 
+    rating: calculateRiskFor(state.prisoner.current.nomsID, JSON.parse(sessionStorage.getItem('viperScores'))) 
 });
 
-export default connect(mapStateToProps, null)(ViolenceRisk);
 
+export { ViolenceRisk };
+
+//Connect HOC
+export default connect(mapStateToProps, null)(ViolenceRisk);
---
+++ tests/components/ViolenceRisk.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { ViolenceRisk } from '../../src/javascript/components/asides/ViolenceRisk';
+
+describe('<ViolenceRisk />', () => {
+    it('renders the component with a viper rating', () => {
+        const wrapper = mount(<ViolenceRisk rating="high"/>);
+        expect(wrapper.find('[data-viper-rating="high"]').length).to.equal(1);
+        
+        expect(wrapper.find('[data-viper-rating]').first().text()).to.equal("high");
+    });
+})
