f41ed438814f63076ef8096f96af571612ec5906
Steven Bapaga <sbapaga@gmail.com>
2017-03-13 16:48:22 +0000

    Add Assessment Complete placeholder page

---
+++ src/javascript/pages/AssessmentComplete.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AssessmentComplete = () => (
+  <div>
+    <h1 className="heading-xlarge">Assessment Complete</h1>
+  </div>
+);
+
+export default AssessmentComplete;
