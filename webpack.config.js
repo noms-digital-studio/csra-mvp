1b7b9e5d7a5a60688eb6b542e58fdacfafe38fa7
Glen Mailer <glenjamin@gmail.com>
2017-03-14 17:11:31 +0000

    Consolidate webpack config and use express server for serving

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,140 @@
-"use strict";
+const path = require('path');
 
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
 
-module.exports = [
-    {
-        devtool: 'cheap-module-source-map',
-        context: __dirname,
-        entry: {
-            'main': './src/javascript/index.js',
-        },
-        stats: {
-            children: false,
-        },
-        output: {
-            path: __dirname + 'dist/',
-            filename: '[name].bundle.js',
-            publicPath: '/'
-        },
-        devServer: {
-            contentBase: __dirname,
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.jsx|\.js$/,
-                    exclude: /node_modules/,
-                    use: [{
-                        loader: 'babel-loader'
-                    }]
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                            plugins: function () {
-                                return [
-                                require('precss'),
-                                require('autoprefixer')
-                                ];
-                            }
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.scss/,
-                    use: [
-                        "style-loader",
-                        {
-                            loader: "css-loader", 
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: true,
-                                includePaths: [
-                                    path.join(__dirname, "node_modules/govuk-elements-sass/public/sass"),
-                                    path.join(__dirname, "node_modules/govuk_frontend_toolkit/stylesheets"),     
-                                ]
-                            }
-                        }
-                    
-                    ]
-                },
-                {
-                    test: /\.(eot|svg|ttf|woff|woff2)$/,
-                    use: ['url-loader?limit=10000']
-                },
-                {
-                    test: /\.(jpe?g|png|gif|svg)$/i,
-                    use: [
-                        "url-loader?limit=10000",
-                        // "file-loader?name=[name].[hash:8].[ext]"
-                    ],
-                }
-            ]
-        },
-        externals: {
-            'jsdom': 'window',
-            'cheerio': 'window',
-            'react/lib/ExecutionEnvironment': true,
-            'react/addons': true,
-            'react/lib/ReactContext': 'window'
-        },
-        resolve: {
-            extensions: ['*', '.js', '.jsx']
-        }
-    }
-]
+const dev = process.env.NODE_ENV !== 'production';
+
+function extractInProduction(loader) {
+  if (dev) {
+    return ['style-loader'].concat(loader);
+  }
+  return ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: loader,
+  });
+}
+
+module.exports = {
+  context: __dirname,
+
+  entry: {
+    main: './src/javascript/index.js',
+  },
+
+  devtool: dev ? 'cheap-module-source-map' : 'source-map',
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: dev ? '[name].bundle.js' : '[name].[hash].js',
+    sourceMapFilename: '[name].[hash].map',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx|\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: extractInProduction([
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [precss, autoprefixer],
+            },
+          },
+        ]),
+      },
+      {
+        test: /\.scss/,
+        loader: extractInProduction([
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              includePaths: [
+                path.join(__dirname, 'node_modules/govuk-elements-sass/public/sass'),
+                path.join(__dirname, 'node_modules/govuk_frontend_toolkit/stylesheets'),
+              ],
+            },
+          },
+        ]),
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'url-loader?limit=10000',
+      },
+    ],
+  },
+
+  plugins: [
+
+    // Remove old artfacts on build
+    new WebpackCleanupPlugin(),
+
+    // Generate the index.html including links to the static assets
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.tmpl.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+
+    // React uses this to do dead code elimination
+    !dev && new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+
+    // CSS is moved into an external file for production
+    !dev && new ExtractTextPlugin('[name].[hash].css'),
+
+    // Minify code in production only
+    !dev && new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: { screw_ie8: true, warnings: false },
+      mangle: { screw_ie8: true },
+      output: { comments: false, screw_ie8: true },
+    }),
+
+  ].filter(Boolean),
+
+  // Shim some things that enzyme requires when running in karma
+  externals: {
+    jsdom: 'window',
+    cheerio: 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/addons': true,
+    'react/lib/ReactContext': 'window',
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
