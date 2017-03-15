9cf5ad98aa9c4bba2714dd146e96c34255c2abf3
Nick Redshaw <nickredshaw@gmail.com>
2017-03-15 12:17:09 +0000

    CSRA-210: Add clear session and local storage button to admin page

--- src/javascript/pages/Admin.jsx
+++ src/javascript/pages/Admin.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 
-import { SIGN_IN } from '../constants/routes.js';
+import routes from '../constants/routes';
 
-import { storeData, readSingleFile } from '../services';
+import {storeData, readSingleFile} from '../services';
 
 class Admin extends Component {
   constructor() {
@@ -14,13 +14,18 @@ class Admin extends Component {
     };
   }
 
-  handleChange({ target: { files, name } }) {
+  handleChange({target: {files, name}}) {
     readSingleFile(files[0], (error, data) => {
-      if (error) return this.setState({ error: true, success: false });
+      if (error) return this.setState({error: true, success: false});
 
       storeData(name, data);
-      this.setState({ error: false, success: true });
+      this.setState({error: false, success: true});
     });
+  }
+
+  clearBrowserData() {
+    sessionStorage.clear();
+    localStorage.clear();
   }
 
   render() {
@@ -28,23 +33,29 @@ class Admin extends Component {
       <div>
         <h1 className="heading-xlarge">Admin</h1>
         <h3 className="heading-medium">Load NOMIS file</h3>
-        <input name="offenderProfiles" onChange={e => this.handleChange(e)} type="file" />
+        <input name="offenderProfiles" onChange={e => this.handleChange(e)} type="file"/>
 
         <h3 className="heading-medium">Load Viper scores file</h3>
-        <input name="viperScores" onChange={e => this.handleChange(e)} type="file" />
+        <input name="viperScores" onChange={e => this.handleChange(e)} type="file"/>
 
         {this.state.error && <div>Whoops something went wrong</div>}
         {this.state.success &&
-          <div>
-            <h2 className="c-message-text c-messgae-text--success">File successfully loaded</h2>
-          </div>}
+        <div>
+          <h2 className="c-message-text c-messgae-text--success">File successfully loaded</h2>
+        </div>}
 
-        {this.state.success &&
-          <Link to={SIGN_IN} className="button c-btn-right--xcustom">Continue to sign in</Link>}
+
+        <h3 className="heading-medium">Reset</h3>
+        <button onClick={() => this.clearBrowserData()} className="button">
+          Clear Browser Session and Local Storage
+        </button>
+
+        <Link to={routes.SIGN_IN} className="button c-btn-right--xcustom">Continue to sign in</Link>
 
       </div>
     );
   }
+
 }
 
-export default LoadData;
+export default Admin;
