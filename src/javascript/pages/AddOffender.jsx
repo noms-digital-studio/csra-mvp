036a9171fcc6df4d5089318a5a749c7fabf02c49
Steven Bapaga <sbapaga@gmail.com>
2017-03-15 12:22:50 +0000

    Add AddOffender test for stand-alone component
    Add gate to stop user from submiting the form without filling it

--- src/javascript/pages/AddOffender.jsx
+++ src/javascript/pages/AddOffender.jsx
@@ -1,101 +1,126 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import serialize from 'form-serialize';
+
 import moment from 'moment';
 import { Link } from 'react-router';
 import routes from '../constants/routes';
 
-const AddOffender = ({ date }) => (
-  <div className="form-section">
-    <p><Link className="link-back" to={routes.DASHBOARD}>Back to dashboard</Link></p>
-    <header>
-      <h1 className="heading-xlarge">
-        <span className="heading-secondary">Add Offender</span>
-        {date}
-      </h1>
-      {/* <div className="grid-row">
-            <div className="column-one-half">
-                <h1 className="heading-large">Add Offender</h1>
-            </div>
-            <div className="column-one-half u-text-align-right">
-                <span className="heading-large u-d-block">{date}</span>
-            </div>
-        </div>*/}
-    </header>
-    <form>
-      <div className="form-group">
-        <label className="form-label-bold" htmlFor="first-name">First name</label>
-        <input className="form-control" name="first-name" type="text" id="first-name" />
-      </div>
-      <div className="form-group">
-        <label className="form-label-bold" htmlFor="last-name">Last name</label>
-        <input className="form-control" name="last-name" type="text" id="last-name" />
-      </div>
-      <div className="form-group">
-        <fieldset>
-          <legend>
-            <span className="form-label-bold">Date of birth</span>
-            <span className="form-hint" id="dob-hint">For example, 31 3 1980</span>
-          </legend>
-          <div className="form-date">
-            <div className="form-group form-group-day">
-              <label className="form-label" htmlFor="dob-day">Day</label>
-              <input
-                className="form-control"
-                id="dob-day"
-                name="dob-day"
-                type="number"
-                pattern="[0-9]*"
-                min="0"
-                max="31"
-                aria-describedby="dob-hint"
-              />
-            </div>
-            <div className="form-group form-group-month">
-              <label className="form-label" htmlFor="dob-month">Month</label>
-              <input
-                className="form-control"
-                id="dob-month"
-                name="dob-month"
-                type="number"
-                pattern="[0-9]*"
-                min="0"
-                max="12"
-              />
-            </div>
-            <div className="form-group form-group-year">
-              <label className="form-label" htmlFor="dob-year">Year</label>
-              <input
-                className="form-control"
-                id="dob-year"
-                name="dob-year"
-                type="number"
-                pattern="[0-9]*"
-                min="0"
-                max="2016"
-              />
-            </div>
-          </div>
-        </fieldset>
-      </div>
-      <div className="form-group">
-        <label className="form-label-bold" htmlFor="nomis-id">Nomis ID</label>
-        <span className="form-hint" id="dob-hint">For example, A5558ZO</span>
-        <input className="form-control" name="nomis-id" type="text" id="nomis-id" />
-      </div>
-      {/* <input type="submit" className="button" value="Add offender" />*/}
-      <Link className="button" to={routes.CONFIRM_OFFENDER}>Add offender</Link>
-    </form>
-  </div>
-  );
+const allFormFieldsComplete = (formData = {}, expectedKeys = []) => {
+  if (Object.keys(formData).length === 0) return false;
 
+  return expectedKeys.reduce((acc, key) => !!formData[key], false);
+};
+
+class AddOffender extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = serialize(event.target, { hash: true });
+    const FormInputNames = Array.from(event.target.elements)
+      .filter(element => element.name !== '')
+      .map(element => element.name);
+
+    if (allFormFieldsComplete(formData, FormInputNames)) {
+      this.props.onSubmit(formData);
+    }
+  }
+
+  render() {
+    const { date } = this.props;
+
+    return (
+      <div className="form-section">
+        <p><Link className="link-back" to={routes.DASHBOARD}>Back to dashboard</Link></p>
+        <header>
+          <h1 className="heading-xlarge">
+            <span className="heading-secondary">Add Offender</span>
+            {date}
+          </h1>
+        </header>
+        <form action="/" method="POST" onSubmit={e => this.handleSubmit(e)}>
+          <div className="form-group">
+            <label className="form-label-bold" htmlFor="first-name">First name</label>
+            <input className="form-control" name="first-name" type="text" id="first-name" />
+          </div>
+          <div className="form-group">
+            <label className="form-label-bold" htmlFor="last-name">Last name</label>
+            <input className="form-control" name="last-name" type="text" id="last-name" />
+          </div>
+          <div className="form-group">
+            <fieldset>
+              <legend>
+                <span className="form-label-bold">Date of birth</span>
+                <span className="form-hint" id="dob-hint">For example, 31 3 1980</span>
+              </legend>
+              <div className="form-date">
+                <div className="form-group form-group-day">
+                  <label className="form-label" htmlFor="dob-day">Day</label>
+                  <input
+                    className="form-control"
+                    id="dob-day"
+                    name="dob-day"
+                    type="number"
+                    pattern="[0-9]*"
+                    min="0"
+                    max="31"
+                    aria-describedby="dob-hint"
+                  />
+                </div>
+                <div className="form-group form-group-month">
+                  <label className="form-label" htmlFor="dob-month">Month</label>
+                  <input
+                    className="form-control"
+                    id="dob-month"
+                    name="dob-month"
+                    type="number"
+                    pattern="[0-9]*"
+                    min="0"
+                    max="12"
+                  />
+                </div>
+                <div className="form-group form-group-year">
+                  <label className="form-label" htmlFor="dob-year">Year</label>
+                  <input
+                    className="form-control"
+                    id="dob-year"
+                    name="dob-year"
+                    type="number"
+                    pattern="[0-9]*"
+                    min="0"
+                    max="2016"
+                  />
+                </div>
+              </div>
+            </fieldset>
+          </div>
+          <div className="form-group">
+            <label className="form-label-bold" htmlFor="nomis-id">Nomis ID</label>
+            <span className="form-hint" id="dob-hint">For example, A5558ZO</span>
+            <input className="form-control" name="nomis-id" type="text" id="nomis-id" />
+          </div>
+          <input type="submit" className="button" value="Add offender" />
+          {/* <Link className="button" to={routes.CONFIRM_OFFENDER}>Add offender</Link>*/}
+        </form>
+      </div>
+    );
+  }
+}
 AddOffender.propTypes = {
   date: PropTypes.string,
   onSubmit: PropTypes.func,
 };
 
-AddOffender.defaultProps = {
+const mapStateToProps = state => ({
   date: moment().format('dddd MMMM DD YYYY'),
-};
+});
+
+const mapActionsToProps = dispatch => ({
+  onSubmit: (data) => {
+    dispatch(push(routes.CONFIRM_OFFENDER));
+  },
+});
 
 export { AddOffender };
-export default connect(null)(AddOffender);
+export default connect(mapStateToProps, mapActionsToProps)(AddOffender);
---
+++ tests/pages/AddOffender.spec.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount, shallow } from 'enzyme';
+
+import { fakeStore } from '../test-helpers';
+
+import ConnectedAddOffender, { AddOffender } from '../../src/javascript/pages/AddOffender';
+
+const profiles = {
+  NOMS_Number: 'foo-id',
+  Surname: 'foo-surname',
+  First_Name: 'foo-first-name',
+  Date_of_Birth: 'foo-age',
+};
+
+describe('<AddOffender />', () => {
+  context('Default AddOffender', () => {
+    it('accepts a date', () => {
+      const date = 'Fooday FooDay FooMonth FooYear';
+      const wrapper = shallow(<AddOffender date={date} />);
+      expect(wrapper.text()).to.include(date);
+    });
+
+    it('fails to submit if fields are missing in the form', () => {
+      const callback = sinon.spy();
+      const wrapper = mount(<AddOffender onSubmit={callback} />);
+
+      wrapper.find('form').simulate('submit');
+
+      expect(callback.calledOnce).to.be.false;
+    });
+
+    it('calls onSubmit callback when for submits successfully', () => {
+      const callback = sinon.spy();
+      const wrapper = mount(<AddOffender onSubmit={callback} />);
+      const expectedCallBackArgument = {
+        'first-name': 'foo-first-name',
+        'last-name': 'foo-last-name',
+        'dob-day': '01',
+        'dob-month': '11',
+        'dob-year': '1960',
+        'nomis-id': 'foo-nomis-id',
+      };
+
+      // Fill Form
+      Object.keys(expectedCallBackArgument).forEach((key) => {
+        wrapper.find(`input[name="${key}"]`).node.value = expectedCallBackArgument[key];
+      });
+
+      wrapper.find('form').simulate('submit');
+
+      expect(callback.calledOnce).to.be.true;
+      expect(callback.calledWith(expectedCallBackArgument));
+    });
+  });
+
+  // context('Connected AddOffender', () => {
+  //   let wrapper;
+  //   let store;
+
+  //   beforeEach(() => {
+  //     store = fakeStore({
+  //       offender: {
+  //         profiles,
+  //       },
+  //     });
+
+  //     wrapper = mount(
+  //       <Provider store={store}>
+  //         <ConnectedAddOffender />
+  //       </Provider>,
+  //     );
+  //   });
+
+  //   it('accepts and correctly renders profiles', () => {
+  //     expect(wrapper.find('[data-profile-row]').length).to.equal(2);
+
+  //     profiles.forEach((profile) => {
+  //       const keys = Object.keys(profile);
+  //       keys.forEach((key) => {
+  //         expect(wrapper.text()).to.include(profile[key]);
+  //       });
+  //     });
+  //   });
+
+  //   it('responds to profile selection', () => {
+  //     const profileBtn = wrapper.find('[data-profile-row]').first().find('button');
+
+  //     profileBtn.simulate('click');
+
+  //     expect(
+  //       store.dispatch.calledWithMatch({ type: 'SELECT_OFFENDER', payload: profiles[0] }),
+  //     ).to.be.true;
+  //   });
+
+  //   it('calls actions when component mounts', () => {
+  //     expect(
+  //       store.dispatch.calledWithMatch({ type: 'GET_VIPER_SCORES', payload: viperScores }),
+  //     ).to.be.true;
+
+  //     expect(
+  //       store.dispatch.calledWithMatch({
+  //         type: 'GET_OFFENDER_NOMIS_PROFILES',
+  //         payload: offenderProfiles.output,
+  //       }),
+  //     ).to.be.true;
+  //   });
+  // });
+});
