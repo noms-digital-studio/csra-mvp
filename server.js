e30ab26c47023d97446c548af1c336e5bf23b349
Glen Mailer <glenjamin@gmail.com>
2017-03-15 12:33:39 +0000

    Add (optional) HTTP basic auth

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require('path');
 
 const express = require('express');
 const morgan = require('morgan');
+const requireAuth = require('./server/require-auth');
 const devBuild = require('./server/dev-build');
 
 // config parsing
 const env = process.env;
 const port = env.PORT || '5000';
 const dev = !env.NODE_ENV || env.NODE_ENV === 'development';
+const authUser = env.BASIC_AUTH_USER;
+const authPass = env.BASIC_AUTH_PASS;
 
 // Express app
 const app = express();
@@ -16,6 +19,10 @@ app.enable('trust proxy');
 app.disable('x-powered-by');
 
 app.use(morgan(dev ? 'dev' : 'short'));
+
+if (authUser) {
+  app.use(requireAuth(authUser, authPass));
+}
 
 if (dev) {
   app.use(devBuild());
