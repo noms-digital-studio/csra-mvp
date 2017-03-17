687f4619e93f1183c07a26216ac148e3d561a8e3
Steven Bapaga <sbapaga@gmail.com>
2017-03-17 16:31:26 +0000

    CSRA-000: Fix formating

--- tests/services/index.spec.js
+++ tests/services/index.spec.js
@@ -1,13 +1,16 @@
-import {calculateRiskFor, viperScores, offenderNomisProfiles, clearBrowserStorage} from '../../src/javascript/services';
+import {
+  calculateRiskFor,
+  viperScores,
+  offenderNomisProfiles,
+  clearBrowserStorage,
+} from '../../src/javascript/services';
 import localViperScores from '../../src/javascript/fixtures/viper.json';
 import localOffenderProfiles from '../../src/javascript/fixtures/nomis.json';
 import testViperScores from '../fixtures/viperScore.json';
 import testOffenderProfiles from '../fixtures/offenderProfiles.json';
 
 describe('Services', () => {
-
   describe('#calculateRiskFor', () => {
-
     it('returns a low risk rating', () => {
       expect(calculateRiskFor('nomisIdForLowRiskPerson1', testViperScores)).to.equal('low');
       expect(calculateRiskFor('nomisIdForLowRiskPerson2', testViperScores)).to.equal('low');
@@ -18,7 +21,7 @@ describe('Services', () => {
       expect(calculateRiskFor('nomisIdForHighRiskPerson2', testViperScores)).to.equal('high');
     });
 
-    context('when a score doesn\'t exist for a given nomisId', () => {
+    context("when a score doesn't exist for a given nomisId", () => {
       it('returns unknown', () => {
         expect(calculateRiskFor('nomisIdForUnknown', testViperScores)).to.equal('unknown');
       });
@@ -26,14 +29,13 @@ describe('Services', () => {
   });
 
   describe('#viperScores', () => {
-
     context('when viperScores are available in browser storage', () => {
       after(() => sessionStorage.clear());
 
       it('returns viper scores from browser storage', () => {
         const scores = JSON.stringify(testViperScores);
 
-        sessionStorage.setItem("viperScores", scores);
+        sessionStorage.setItem('viperScores', scores);
 
         expect(viperScores()).to.eql(JSON.parse(scores));
       });
@@ -44,22 +46,19 @@ describe('Services', () => {
         expect(viperScores()).to.eql(localViperScores);
       });
     });
-
   });
 
   describe('#offenderNomisProfiles', () => {
-
     context('when offenderNomisProfiles is available in browser storage', () => {
       after(() => sessionStorage.clear());
 
       it('returns offender NOMIS data form browser storage', () => {
         const offenderProfiles = JSON.stringify(testOffenderProfiles);
 
-        sessionStorage.setItem("offenderProfiles", offenderProfiles);
+        sessionStorage.setItem('offenderProfiles', offenderProfiles);
 
         expect(offenderNomisProfiles()).to.eql(JSON.parse(offenderProfiles));
-
-      })
+      });
     });
 
     context('when offenderNomisProfiles is unavailable in browser storage', () => {
@@ -67,22 +66,17 @@ describe('Services', () => {
         expect(offenderNomisProfiles()).to.eql(localOffenderProfiles);
       });
     });
-
   });
 
   describe('#clearBrowserStorage', () => {
+    it('clears browser session and local storage', () => {
+      sessionStorage.setItem('foo', 'bar');
+      localStorage.setItem('sna', 'fu');
 
-      it('clears browser session and local storage', () => {
+      clearBrowserStorage();
 
-          sessionStorage.setItem('foo', 'bar');
-          localStorage.setItem('sna', 'fu');
-
-          clearBrowserStorage();
-
-          expect(sessionStorage.length).to.eql(0);
-          expect(localStorage.length).to.eql(0);
-
-        })
-      });
-
+      expect(sessionStorage.length).to.eql(0);
+      expect(localStorage.length).to.eql(0);
+    });
+  });
 });
