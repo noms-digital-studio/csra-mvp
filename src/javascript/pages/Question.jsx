e1333daecd482ed15d30cb0e380746fdb4437456
Steven Bapaga <sbapaga@gmail.com>
2017-03-17 16:36:21 +0000

    CSRA-192: Add ability to store answers in Question component
    Refactored onSubmit logic by delaging redictions and action to connect
    Add much needed tests for the <Question />

--- src/javascript/pages/Question.jsx
+++ src/javascript/pages/Question.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { hashHistory } from 'react-router';
+import { push } from 'react-router-redux';
+import serialize from 'form-serialize';
 
 import { isEmpty } from 'ramda';
 
 import Routes from '../constants/routes';
-import { getQuestions, storeAnswer } from '../actions';
+import { getQuestions, saveAnswer } from '../actions';
 
 import QuestionWithAsideTemplate from '../containers/QuestionWithAside';
 import QuestionWithComments from '../containers/QuestionWithComments';
 import ConfirmationTemplate from '../containers/Confirmation';
 import ConfirmationWithAsideTemplate from '../containers/ConfirmationWithAside';
 import HealthcareAssessment from '../containers/HealthAssessment';
-
 
 function templateSelector(data) {
   switch (data.template) {
@@ -31,23 +31,31 @@ function templateSelector(data) {
   }
 }
 
-
 class Question extends Component {
   componentDidMount() {
-        // Temporary
     this.props.getQuestions();
   }
 
-  handleFormSubmit(e) {
-    e.preventDefault();
-    const { sectionIndex } = this.sectionData(this.props.questions, this.props.params.section);
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    const { sectionIndex, question } = this.sectionData(
+      this.props.questions,
+      this.props.params.section,
+    );
+    const answer = serialize(event.target, { hash: true });
+    const basePath = Routes.ASSESSMENT;
     const nextSectionIndex = sectionIndex + 1;
+
+    let nextPath;
+
     if (this.props.questions[nextSectionIndex]) {
-      const nextQuestion = this.props.questions[nextSectionIndex];
-      return hashHistory.push({ pathname: `${Routes.ASSESSMENT}/${nextQuestion.riskIndicator}` });
+      nextPath = `${basePath}/${this.props.questions[nextSectionIndex].riskIndicator}`;
+    } else {
+      nextPath = Routes.SUMMARY;
     }
 
-    return hashHistory.push({ pathname: Routes.SUMMARY });
+    this.props.onSubmit(question.riskIndicator, answer, nextPath);
   }
 
   sectionData(questions = [], section = '') {
@@ -60,9 +68,10 @@ class Question extends Component {
     }
     const sectionEqls = item => item.riskIndicator === section;
     const index = questions.findIndex(sectionEqls);
+
     const total = questions.length;
     const question = questions.find(sectionEqls);
-    const adJustedIndex = (index !== undefined) ? index : 0;
+    const adJustedIndex = index !== undefined ? index : 0;
 
     return {
       totalSections: total,
@@ -73,10 +82,10 @@ class Question extends Component {
 
   render() {
     const {
-            questions,
-            params: { section },
-            selectedOffender: { firstName, surname },
-        } = this.props;
+      questions,
+      params: { section },
+      prisoner: { firstName, surname },
+    } = this.props;
 
     const { totalSections, sectionIndex, question } = this.sectionData(questions, section);
 
@@ -84,39 +93,63 @@ class Question extends Component {
       <div className="o-question">
         <div className="grid-row">
           <div className="column-half">
-            {/* <h2 className="c-section-title">Section {sectionIndex + 1} of {totalSections}</h2>*/}
-            <h2 className="c-section-title">
-              <span>Section &nbsp;</span>
+            <h2
+              data-section-holder={`Section ${sectionIndex + 1} of ${totalSections}`}
+              className="c-section-title"
+            >
+              <span>Section&nbsp;</span>
               <span className="circle circle-step">{sectionIndex + 1}</span>
-              <span>&nbsp; of &nbsp;</span>
+              <span>&nbsp;of&nbsp;</span>
               <span className="circle circle-step">{totalSections}</span>
             </h2>
 
           </div>
           <div className="column-half">
-            <h2 className="bold-medium u-text-align-right" id="subsection-title">{firstName} {surname}</h2>
+            <h2 className="bold-medium u-text-align-right" id="subsection-title">
+              {firstName} {surname}
+            </h2>
           </div>
         </div>
-        { templateSelector({ ...question, onSubmit: e => this.handleFormSubmit(e) }) }
+        {templateSelector({ ...question, onSubmit: e => this.handleFormSubmit(e) })}
       </div>
     );
   }
 }
 
+Question.propTypes = {
+  questions: PropTypes.array,
+  params: PropTypes.object,
+  prisoner: PropTypes.object,
+  getQuestions: PropTypes.func,
+  onSubmit: PropTypes.func,
+};
+
+Question.defaultProps = {
+  questions: [],
+  params: {},
+  prisoner: {},
+  getQuestions: () => {},
+  onSubmit: () => {},
+};
 
 const mapStateToProps = (state, ownProps) => ({
   ...ownProps,
   questions: state.questions.questions,
-  selectedOffender: {
+  prisoner: {
     firstName: state.offender.selected.First_Name,
     surname: state.offender.selected.Surname,
   },
 });
 
+const mapActionsToProps = dispatch => ({
+  getQuestions: () => {
+    dispatch(getQuestions());
+  },
+  onSubmit: (riskIndicator, answer, nextPath) => {
+    dispatch(saveAnswer(riskIndicator, answer));
+    dispatch(push(nextPath));
+  },
+});
 
-Question.defaultProps = {
-  profileFirstName: 'John',
-  profileLastName: 'Smith',
-};
-
-export default connect(mapStateToProps, { getQuestions })(Question);
+export { Question };
+export default connect(mapStateToProps, mapActionsToProps)(Question);
